d955c7917dfdfff3777d8665d2225665c77cadbd
JBorrow <joshua.borrow@durham.ac.uk>
2014-12-15 16:49:19 +0000

    added footer

---
+++ footer.php
@@ -0,0 +1,11 @@
+<div class="getsinway"></div>
+
+<footer>
+    <a href="http://www.intel.co.uk"><img src="<?php bloginfo('template_url');?>/logos/intel.png" alt="Intel" /></a>
+    <a href="http://icc.dur.ac.uk"><img src="<?php bloginfo('template_url');?>/logos/icc.png" alt="Institute for Computational Cosmology" /></a>
+    <a href="http://www.dur.ac.uk"><img src="<?php bloginfo('template_url');?>/logos/durham.png" alt="Durham University" /></a>
+</footer>
+
+</body>
+
+</html>
