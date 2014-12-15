e67ab58bad161976898678b2c72b54e419ea3687
JBorrow <joshua.borrow@durham.ac.uk>
2014-12-15 16:47:48 +0000

    Added basic header file

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title><?php bloginfo('title');?></title>
+        <link rel="stylesheet" href="<?php bloginfo('stylesheet_url');?>" type="text/css" media="screen"/>
+        <?php wp_head();?>
+    </head>
+
+<body>
+    <div class="header">
+        <h1><?php bloginfo('name') ?></h1>
+        <h2><?php bloginfo('description') ?></h2>
+    </div>
+	<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'navbar' ) ); ?>
+
